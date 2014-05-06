5b84f386cff2993a66652132c8a09eacde2ad1ca
MulajinDev <damianoleary1@hotmail.com>
2014-05-06 17:53:19 -0400

    commented controls.js

--- js/Controls.js
+++ js/Controls.js
@@ -15,23 +15,22 @@ var BasicGame;
     }
 
     Controls.prototype.create = function () {
-        console.log('Credits');
-        var background = this.add.image(0,0, 'menu_bg');
-        background.scale.setTo(0.65, 0.75);
-      	this.backButton = this.add.button(50, 50, 'controls_button', this.backToMenu, this, 1,0,1);
-        var i = 0;
-        var text = "Controls:\nControl Direction by moving mouse\nClick to fire";
+      //announce credits starting
+      console.log('Credits');
+      
+      //set and display controls assets
+      var background = this.add.image(0,0, 'menu_bg');
+      background.scale.setTo(0.65, 0.75);
+      his.backButton = this.add.button(50, 50, 'controls_button', this.backToMenu, this, 1,0,1);
+      var i = 0;
+      var text = "Controls:\nControl Direction by moving mouse\nClick to fire";
     	var style = { font: "45px Helvetica Neue UltraLight", fill: "#FCFF00", align: "center" };
-
     	var t = this.add.text(this.world.centerX-250, this.world.centerY+75, text, style);
     	
     };
-
-    Controls.prototype.update = function() {
-
-    };
     
     Controls.prototype.backToMenu = function () {
+      //return to main menut when clicked
     	this.game.state.start("MainMenu");
     }
 
